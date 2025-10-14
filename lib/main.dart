09aae4d2de654fe80d62fe62cc6c3d3d82a71d1c
Saon00 <sikdersaon1@gmail.com>
2025-10-14 23:25:05 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,30 @@
-import 'package:coaching_beton/routes/routes.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
+  import 'package:coaching_beton/routes/routes.dart';
+  import 'package:flutter/material.dart';
+  import 'package:flutter_screenutil/flutter_screenutil.dart';
+  import 'package:get/get.dart';
+  import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (_, child) {
-        return GetMaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-          routerDelegate: AppRoutes().route.routerDelegate,
-          routeInformationParser: AppRoutes().route.routeInformationParser,
-        );
-      },
-    );
+  void main() {
+    runApp(const MainApp());
   }
-}
+
+  class MainApp extends StatelessWidget {
+    const MainApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return GetMaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+            routerDelegate: AppRoutes().route.routerDelegate,
+            routeInformationParser: AppRoutes().route.routeInformationParser,
+          );
+        },
+      );
+    }
+  }
